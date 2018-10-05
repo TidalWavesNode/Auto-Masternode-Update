cd6d24dc2bb8c52259f850ff6e8633c4d46d6839
Tidal Waves <33072338+TidalWavesNode@users.noreply.github.com>
2018-10-05 06:51:02 -0700

    Update MNupdate.sh

--- MNupdate.sh
+++ MNupdate.sh
@@ -10,9 +10,8 @@ rm -r ~/wagerr-2.0.0
 wget -q https://github.com/wagerr/wagerr/releases/download/v2.0.1/wagerr-2.0.1-x86_64-linux-gnu.tar.gz
 tar -xvf wagerr-2.0.1-x86_64-linux-gnu.tar.gz
 rm wagerr-2.0.1-x86_64-linux-gnu.tar.gz
+
 #start wagerrd
 cd ~/wagerr-2.0.1/bin
 ./wagerrd
-./wagerr-cli masternode status
-./wagerr-cli getinfo
 echo -e "Your Wagerr Masternode has been successfully upgraded to version 2.0.1 Monte Carlo"
