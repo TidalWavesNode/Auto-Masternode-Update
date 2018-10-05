c16ceb9c06b8e4875765642e6718348799fdfcbd
Tidal Waves <33072338+TidalWavesNode@users.noreply.github.com>
2018-10-05 06:45:38 -0700

    Update MNupdate.sh

--- MNupdate.sh
+++ MNupdate.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#prepare MN for updating by stopping wagerrd
+#prepare MN for updating by stopping wagerrd and removing wagerr-2.0.0
 cd ~/wagerr-2.0.0/bin
 ./wagerr-cli stop
-#download, unzip, and remove tar.gz for new new release
-cd ..
-cd ..
+cd ~
 rm -r ~/wagerr-2.0.0
+
+#download, unzip, and remove tar.gz for new new release
 wget -q https://github.com/wagerr/wagerr/releases/download/v2.0.1/wagerr-2.0.1-x86_64-linux-gnu.tar.gz
 tar -xvf wagerr-2.0.1-x86_64-linux-gnu.tar.gz
 rm wagerr-2.0.1-x86_64-linux-gnu.tar.gz
